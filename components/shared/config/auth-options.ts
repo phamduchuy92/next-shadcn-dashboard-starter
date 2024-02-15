c9b6b354ec68ff6c79225b08be83f6b6e8e2ad21
huypd <huypd@vng.com.vn>
2024-02-15 11:08:15 +0700

    add formly

--- components/shared/config/auth-options.ts
+++ components/shared/config/auth-options.ts
@@ -10,27 +10,40 @@ export const authOptions: NextAuthOptions = {
     }),
     CredentialProvider({
       credentials: {
-        email: {
-          label: "email",
-          type: "email",
-          placeholder: "example@gmail.com",
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "admin",
+        },
+        password: {
+          label: "Password",
+          type: "password",
         },
       },
       async authorize(credentials, req) {
-        const user = { id: "1", name: "John", email: credentials?.email };
+        const res = await fetch(`http://localhost:8000/api/v1/authenticate`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: credentials?.username,
+            password: credentials?.password
+          })
+        })
+        const user = await res.json();
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
           return user;
         } else {
           // If you return null then an error will be displayed advising the user to check their details.
           return null;
-
           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
       },
     }),
   ],
   pages: {
-    signIn: "/", //sigin page
+    signIn: "/", //signin page
   },
 };
